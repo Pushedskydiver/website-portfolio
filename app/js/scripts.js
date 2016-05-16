f14762bd9dc62f5de0feb48992c1360760fadbd2
Alex Clapperton <Pushedskydiver@users.noreply.github.com>
2016-05-16 21:14:04 +0100

    Changed colour scheme. Added blog section.

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -120,17 +120,78 @@
     // ======================================
     //    Particle ground
     // ======================================
-    function init_particle_ground() {
-      $('.site-banner').particleground({
-          dotColor: '#FF2B42',
-          lineColor: '#3C3744',
-          density: 10000,
-          proximity: 100,
-          particleRadius: 5,
-          parallax: true,
-          parallaxMultiplier: 20
-      });
-    }
+    particlesJS("particles-js", {
+			"particles": {
+				"number": {
+					"value":223, "density": {
+						"enable": true, "value_area": 800
+					}
+				}
+				, "color": {
+					"value": "#ffffff"
+				}
+				, "shape": {
+					"type":"circle", "stroke": {
+						"width": 0, "color": "#000000"
+					}
+					, "polygon": {
+						"nb_sides": 5
+					}
+					, "image": {
+						"src": "img/github.svg", "width": 100, "height": 100
+					}
+				}
+				, "opacity": {
+					"value":1, "random":true, "anim": {
+						"enable": true, "speed": 1, "opacity_min": 0, "sync": false
+					}
+				}
+				, "size": {
+					"value":2, "random":true, "anim": {
+						"enable": false, "speed": 4, "size_min": 0.3, "sync": false
+					}
+				}
+				, "line_linked": {
+					"enable": false, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1
+				}
+				, "move": {
+					"enable":true, "speed":2.5, "direction":"top", "random":true, "straight":false, "out_mode":"out", "bounce":false, "attract": {
+						"enable": false, "rotateX": 600, "rotateY": 600
+					}
+				}
+			}
+			, "interactivity": {
+				"detect_on":"canvas", "events": {
+					"onhover": {
+						"enable": true, "mode": "bubble"
+					}
+					, "onclick": {
+						"enable": true, "mode": "repulse"
+					}
+					, "resize":true
+				}
+				, "modes": {
+					"grab": {
+						"distance":400, "line_linked": {
+							"opacity": 1
+						}
+					}
+					, "bubble": {
+						"distance": 250, "size": 0, "duration": 2, "opacity": 0, "speed": 3
+					}
+					, "repulse": {
+						"distance": 400, "duration": 0.4
+					}
+					, "push": {
+						"particles_nb": 4
+					}
+					, "remove": {
+						"particles_nb": 2
+					}
+				}
+			}
+			, "retina_detect":true
+		});
 
 
     // ======================================
@@ -258,7 +319,8 @@
         offset: 0, // distance to the element when triggering the animation (default is 0)
         mobile: false, // trigger animations on mobile devices (default is true)
         live: true // act on asynchronously loaded content (default is true)
-      })
+      });
+      wow.init();
     }
 
 
@@ -275,7 +337,6 @@
     if (!$('body').hasClass('mobile')) {
       init_smooth_scroll_top();
       init_nav_bg_change();
-      init_particle_ground();
       init_wow();
     }
 
